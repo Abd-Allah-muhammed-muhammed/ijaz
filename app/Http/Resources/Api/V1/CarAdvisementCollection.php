0c7fd2fe00fc3045c2284b3cdd59b75eed097bd0
Ahmed Essam <aahmedessam30@gmail.com>
2026-05-18 18:29:46 +0300

    refactor: Simplify CarAdvisementFilters constructor and enhance filter handling

--- app/Http/Resources/Api/V1/CarAdvisementCollection.php
+++ app/Http/Resources/Api/V1/CarAdvisementCollection.php
@@ -6,12 +6,5 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class CarAdvisementCollection extends ResourceCollection
 {
-    public $collects = CarAdvisementResource::class;
-
-    public function toArray($request): array
-    {
-        return [
-            'data' => $this->collection,
-        ];
-    }
+  public $collects = CarAdvisementResource::class;
 }
