64dd12c182b77a1a9bd550b6a2d96344d16957df
Ahmed Essam <aahmedessam30@gmail.com>
2026-07-19 14:56:14 +0300

    refactor(routes): add authentication middleware to chat routes for Guarantor and Opportunity modules

--- Modules/Opportunity/Routes/V1/chat.php
+++ Modules/Opportunity/Routes/V1/chat.php
@@ -3,9 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Opportunity\Http\Controllers\V1\OpportunityChatController;
 
-Route::prefix('opportunities')->name('chats.opportunities.')->controller(OpportunityChatController::class)->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::post('/', 'store')->name('store');
-    Route::get('{conversation}', 'show')->name('show');
-    Route::post('{conversation}/send', 'send')->name('send');
+Route::middleware('auth:sanctum')->group(static function () {
+    Route::prefix('chats')->group(static function () {
+        Route::prefix('opportunities')->name('chats.opportunities.')->controller(OpportunityChatController::class)->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::post('/', 'store')->name('store');
+            Route::get('{conversation}', 'show')->name('show');
+            Route::post('{conversation}/send', 'send')->name('send');
+        });
+    });
 });
