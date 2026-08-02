4ff37a29b0b61485d9ce9bfcadd9ca0101d3fa04
Ahmed Essam <aahmedessam30@gmail.com>
2026-08-03 00:16:43 +0300

    fix(provider): update logo image styling in AccountLayout and ProviderCard for better fit and appearance

--- resources/js/apps/provider/layouts/AccountLayout.tsx
+++ resources/js/apps/provider/layouts/AccountLayout.tsx
@@ -73,7 +73,7 @@ const AccountLayout = ({children, provider}: Props) => {
             <div className='d-flex flex-wrap flex-sm-nowrap mb-3'>
               <div className='me-7 mb-4'>
                 <div className='symbol symbol-100px symbol-lg-160px symbol-fixed position-relative'>
-                  <img src={provider.logo} alt='Metronic'/>
+                  <img src={provider.logo} alt='Metronic' className='object-fit-contain'/>
                   <div
                     className='position-absolute translate-middle bottom-0 start-100 mb-6 bg-success rounded-circle border-4 border-white h-20px w-20px'></div>
                 </div>
